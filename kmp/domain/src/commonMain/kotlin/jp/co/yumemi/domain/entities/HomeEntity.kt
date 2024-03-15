55883590951f0788d2029fda22c4a325a1d8b5ec
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-15 09:03:19 +0100

    feat: create home entity

---
+++ kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/entities/HomeEntity.kt
@@ -0,0 +1,17 @@
+package jp.co.yumemi.domain.entities
+
+import jp.co.yumemi.utils.Parcelable
+import jp.co.yumemi.utils.Parcelize
+
+/**
+ * ホーム画面 一覧のアイテム
+ */
+@Parcelize
+data class HomeEntity(
+    /** 作品のタイトル */
+    val title: String,
+    /** 作品のリリース時期 */
+    val seasonName: String,
+    /** 作品のイメージ画像 */
+    val imageUrl: String?,
+) : Parcelable
