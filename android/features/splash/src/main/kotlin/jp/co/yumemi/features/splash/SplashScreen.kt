1ad85778c0efc74bd6c18bca1a37fc12f345940e
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-15 08:41:52 +0100

    fix: delete unused import

--- android/features/splash/src/main/kotlin/jp/co/yumemi/features/splash/SplashScreen.kt
+++ android/features/splash/src/main/kotlin/jp/co/yumemi/features/splash/SplashScreen.kt
@@ -1,27 +1,20 @@
 package jp.co.yumemi.features.splash
 
-import android.window.SplashScreen
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import jp.co.yumemi.core.primitives.SampleTheme
-import jp.co.yumemi.core.utils.render
 import jp.co.yumemi.core.utils.screenPadding
-import jp.co.yumemi.splash.SplashState
 import jp.co.yumemi.core.R
-import jp.co.yumemi.splash.SplashEvent
-import jp.co.yumemi.splash.SplashIntent
 
 @Composable
 fun SplashScreenRoot(
