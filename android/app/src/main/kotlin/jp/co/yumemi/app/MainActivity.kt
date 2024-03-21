b346787f9de02118c1941583ac34fbbaa08e4ebc
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-21 11:00:58 +0100

    feat: Display the splash screen when starting the app

--- android/app/src/main/kotlin/jp/co/yumemi/app/MainActivity.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/app/MainActivity.kt
@@ -1,16 +1,25 @@
 package jp.co.yumemi.app
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import jp.co.yumemi.app.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private var splashScreen: SplashScreen? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Android12以降でシステムのスプラッシュを長く表示する対応
+        if (Build.VERSION.SDK_INT >= 31) {
+            splashScreen = installSplashScreen()
+        }
+
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
