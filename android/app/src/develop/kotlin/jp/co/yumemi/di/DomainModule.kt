fd0531783462b3d716e40e3fc4e6f5b2803377ab
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-21 12:01:41 +0100

    feat: add domain module

--- android/app/src/develop/kotlin/jp/co/yumemi/di/DomainModule.kt
+++ android/app/src/develop/kotlin/jp/co/yumemi/di/DomainModule.kt
@@ -5,7 +5,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
 import dagger.hilt.android.components.ServiceComponent
+import jp.co.yumemi.domain.repositories.HomeRepository
 import jp.co.yumemi.domain.repositories.SampleRepository
+import jp.co.yumemi.domain.usecases.HomeExecutor
+import jp.co.yumemi.domain.usecases.HomeUseCase
 import jp.co.yumemi.domain.usecases.SampleExecutor
 import jp.co.yumemi.domain.usecases.SampleUseCase
 
@@ -16,4 +19,9 @@ class DomainModule {
     fun provideSampleUseCase(
         sampleRepository: SampleRepository,
     ): SampleUseCase = SampleExecutor(sampleRepository = sampleRepository)
+
+    @Provides
+    fun provideHomeUseCase(
+        homeRepository: HomeRepository,
+    ): HomeUseCase = HomeExecutor(homeRepository = homeRepository)
 }
