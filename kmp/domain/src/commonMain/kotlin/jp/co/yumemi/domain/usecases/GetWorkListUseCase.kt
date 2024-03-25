5e912b25ea7f879e540d4493b17f0fb652440165
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-25 10:21:45 +0100

    feat: delete Args

--- kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/usecases/GetWorkListUseCase.kt
+++ kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/usecases/GetWorkListUseCase.kt
@@ -4,12 +4,10 @@ import jp.co.yumemi.domain.core.UseCase
 import jp.co.yumemi.domain.entities.WorkEntity
 import jp.co.yumemi.domain.repositories.WorkRepository
 
-interface HomeUseCase : UseCase<HomeUseCase.Args, List<WorkEntity>> {
-    data class Args(val id: Int)
-}
+interface GetWorkListUseCase : UseCase<Unit, List<WorkEntity>>
 
-class HomeExecutor(
+class GetWorkListExecutor(
     private val workRepository: WorkRepository,
-) : HomeUseCase {
-    override suspend fun execute(arguments: HomeUseCase.Args): List<WorkEntity> = workRepository.getHomeWorkList(id = arguments.id)
+) : GetWorkListUseCase {
+    override suspend fun execute(arguments: Unit): List<WorkEntity> = workRepository.getWorkList()
 }
