8d74f01ca5a5f4fc65dbfeb617b9e2995998f444
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-25 06:03:06 +0100

    feat: edit home usecase

--- kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/usecases/HomeUseCase.kt
+++ kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/usecases/HomeUseCase.kt
@@ -4,12 +4,12 @@ import jp.co.yumemi.domain.core.UseCase
 import jp.co.yumemi.domain.entities.HomeEntity
 import jp.co.yumemi.domain.repositories.HomeRepository
 
-interface HomeUseCase : UseCase<HomeUseCase.Args, HomeEntity> {
+interface HomeUseCase : UseCase<HomeUseCase.Args, List<HomeEntity>> {
     data class Args(val id: Int)
 }
 
 class HomeExecutor(
     private val homeRepository: HomeRepository,
 ) : HomeUseCase {
-    override suspend fun execute(arguments: HomeUseCase.Args): HomeEntity = homeRepository.getHomeWorkList(id = arguments.id)
+    override suspend fun execute(arguments: HomeUseCase.Args): List<HomeEntity> = homeRepository.getHomeWorkList(id = arguments.id)
 }
