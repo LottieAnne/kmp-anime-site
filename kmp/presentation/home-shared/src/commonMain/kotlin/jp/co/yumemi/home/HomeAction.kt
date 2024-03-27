3d13cf32379930e2a5d32f77594eba2f4e61515a
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-27 07:20:22 +0100

    fix: typo

--- kmp/presentation/home-shared/src/commonMain/kotlin/jp/co/yumemi/home/HomeAction.kt
+++ kmp/presentation/home-shared/src/commonMain/kotlin/jp/co/yumemi/home/HomeAction.kt
@@ -4,6 +4,6 @@ import jp.co.yumemi.domain.entities.WorkEntity
 import tech.fika.macaron.core.contract.Action
 
 sealed class HomeAction : Action {
-    data object GetWorList : HomeAction()
+    data object GetWorkList : HomeAction()
     data class NavigateDetails(val listItem: WorkEntity) : HomeAction()
 }
