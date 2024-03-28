0a020d0d4c6e7a46940b66f1865e071093894f99
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-28 09:48:45 +0100

    feat: add view model and contract

--- android/features/home/src/main/kotlin/jp/co/yumemi/features/home/HomeFragment.kt
+++ android/features/home/src/main/kotlin/jp/co/yumemi/features/home/HomeFragment.kt
@@ -5,16 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.core.foundation.contract
 import jp.co.yumemi.core.utils.composeView
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View = composeView {
-        HomeScreenRoot()
+        HomeScreenRoot(
+            contract = contract(store = viewModel.store),
+        )
     }
 }
