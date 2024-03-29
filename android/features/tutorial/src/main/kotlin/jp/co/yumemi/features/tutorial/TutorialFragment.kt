e6c50a038de307d94db09eac4e2a0a0ac25556e4
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-29 12:24:09 +0100

    feat: create tutorial fragment

--- android/features/tutorial/src/main/kotlin/jp/co/yumemi/features/tutorial/TutorialFragment.kt
+++ android/features/tutorial/src/main/kotlin/jp/co/yumemi/features/tutorial/TutorialFragment.kt
@@ -1,4 +1,32 @@
 package jp.co.yumemi.features.tutorial
 
-class TutorialFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.core.foundation.contract
+import jp.co.yumemi.core.foundation.setNavController
+import jp.co.yumemi.core.utils.composeView
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class TutorialFragment: Fragment() {
+    @Inject
+    lateinit var navigator: TutorialNavigator
+
+    private val viewModel: TutorialViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View = composeView {
+        TutorialScreenRoot(
+            contract = contract(store = viewModel.store),
+            navigator = navigator.setNavController(),
+        )
+    }
 }
