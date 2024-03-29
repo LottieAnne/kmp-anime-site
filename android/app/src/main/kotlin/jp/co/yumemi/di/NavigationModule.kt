9bfb30545dacd689379464c8b456623588b1767a
LottieAnne <a_fukui@yumemi.co.jp>
2024-03-29 12:22:54 +0100

    feat: add di > tutorial navigation module

--- android/app/src/main/kotlin/jp/co/yumemi/di/NavigationModule.kt
+++ android/app/src/main/kotlin/jp/co/yumemi/di/NavigationModule.kt
@@ -6,8 +6,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
 import jp.co.yumemi.features.sample.SampleNavigator
 import jp.co.yumemi.features.splash.SplashNavigator
+import jp.co.yumemi.features.tutorial.TutorialNavigator
 import jp.co.yumemi.navigation.SampleNavigation
 import jp.co.yumemi.navigation.SplashNavigation
+import jp.co.yumemi.navigation.TutorialNavigation
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
@@ -17,4 +19,7 @@ class NavigationModule {
 
     @Provides
     fun provideSplashNavigator(): SplashNavigator = SplashNavigation()
+
+    @Provides
+    fun provideTutorialNavigator(): TutorialNavigator = TutorialNavigation()
 }
