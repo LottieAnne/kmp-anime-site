57f26fe62f2df772cce023367ee3efc02a165c53
LottieAnne <a_fukui@yumemi.co.jp>
2024-04-01 19:29:45 +0200

    feat: add work details action

--- kmp/presentation/work-details-shared/src/commonMain/kotlin/jp/co/yumemi/workDetails/WorkDetailsAction.kt
+++ kmp/presentation/work-details-shared/src/commonMain/kotlin/jp/co/yumemi/workDetails/WorkDetailsAction.kt
@@ -2,4 +2,7 @@ package jp.co.yumemi.workDetails
 
 import tech.fika.macaron.core.contract.Action
 
-sealed class WorkDetailsAction: Action {}
+sealed class WorkDetailsAction : Action {
+    data object GetWorkDetailsList : WorkDetailsAction()
+    data object GetWorkEpisodeList: WorkDetailsAction()
+}
