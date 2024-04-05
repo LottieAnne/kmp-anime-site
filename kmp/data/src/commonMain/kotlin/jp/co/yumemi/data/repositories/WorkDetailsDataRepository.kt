5efe07abdc1b4df1e1241ad51b01fab55fd2c82c
LottieAnne <a_fukui@yumemi.co.jp>
2024-04-05 19:27:38 +0200

    feat: add data

--- kmp/data/src/commonMain/kotlin/jp/co/yumemi/data/repositories/WorkDetailsDataRepository.kt
+++ kmp/data/src/commonMain/kotlin/jp/co/yumemi/data/repositories/WorkDetailsDataRepository.kt
@@ -1,30 +1,19 @@
 package jp.co.yumemi.data.repositories
 
 import jp.co.yumemi.data.mappers.WorkEpisodeListDataMapper
-import jp.co.yumemi.data.mappers.WorkInfoDataMapper
-import jp.co.yumemi.data.mappers.WorkListDataMapper
-import jp.co.yumemi.data.sources.WorkDetailsRemoteDataSource
-import jp.co.yumemi.data.sources.WorkListRemoteDataSource
-import jp.co.yumemi.domain.entities.WorkEntity
+import jp.co.yumemi.data.sources.WorkEpisodeListRemoteDataSource
 import jp.co.yumemi.domain.entities.WorkEpisodeEntity
-import jp.co.yumemi.domain.entities.WorkInfoEntity
 import jp.co.yumemi.domain.repositories.WorkDetailsRepository
-import jp.co.yumemi.domain.repositories.WorkListRepository
 import kotlinx.coroutines.delay
 
 class WorkDetailsDataRepository(
-    private val workDetailsRemoteDataSource: WorkDetailsRemoteDataSource,
+    private val workEpisodeListRemoteDataSource: WorkEpisodeListRemoteDataSource,
 ) : WorkDetailsRepository {
-    override suspend fun getWorkInfo(): WorkInfoEntity {
-        // Mock
-        delay(2_000L)
-        return workDetailsRemoteDataSource.getWorkInfo().let(WorkInfoDataMapper::toEntity)
-    }
 
-    override suspend fun getWorkEpisodeList(): List<WorkEpisodeEntity> {
+    override suspend fun getWorkEpisodeList(id: Int): List<WorkEpisodeEntity> {
         // Mock
         delay(2_000L)
-        return workDetailsRemoteDataSource.getWorkEpisodeList()
+        return workEpisodeListRemoteDataSource.getWorkEpisodeList(id)
             .map(WorkEpisodeListDataMapper::toEntity)
     }
 }
--- kmp/data/src/commonMain/kotlin/jp/co/yumemi/data/sources/WorkEpisodeListRemoteDataSource.kt
+++ kmp/data/src/commonMain/kotlin/jp/co/yumemi/data/sources/WorkEpisodeListRemoteDataSource.kt
@@ -2,6 +2,6 @@ package jp.co.yumemi.data.sources
 
 import jp.co.yumemi.data.models.WorkEpisodeListModel
 
-interface WorkDetailsRemoteDataSource {
+interface WorkEpisodeListRemoteDataSource {
     suspend fun getWorkEpisodeList(id: Int): List<WorkEpisodeListModel>
 }
