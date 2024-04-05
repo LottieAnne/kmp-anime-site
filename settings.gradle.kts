65e4ac47ce754271468a5b7bbdf6cd1d322579f0
LottieAnne <a_fukui@yumemi.co.jp>
2024-04-05 11:07:28 +0200

    chore: create work details module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -52,6 +52,7 @@ include(":kmp:presentation:sample-shared")
 include(":kmp:presentation:tutorial-shared")
 include(":kmp:presentation:home-shared")
 include(":kmp:presentation:splash-shared")
+include(":kmp:presentation:work-details-shared")
 
 fun getProperty(key: String, default: String? = null): String = System.getProperty(key)
     ?: System.getenv(key)
