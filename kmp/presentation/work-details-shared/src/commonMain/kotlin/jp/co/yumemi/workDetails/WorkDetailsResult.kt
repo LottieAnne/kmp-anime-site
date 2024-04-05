724bad91be91f1ace98640241338a457f5878184
LottieAnne <a_fukui@yumemi.co.jp>
2024-04-05 19:00:40 +0200

    feat: add constract

--- kmp/presentation/work-details-shared/src/commonMain/kotlin/jp/co/yumemi/workDetails/WorkDetailsResult.kt
+++ kmp/presentation/work-details-shared/src/commonMain/kotlin/jp/co/yumemi/workDetails/WorkDetailsResult.kt
@@ -1,16 +1,13 @@
 package jp.co.yumemi.workDetails
 
 import jp.co.yumemi.domain.core.DomainError
-import jp.co.yumemi.domain.entities.WorkEntity
 import jp.co.yumemi.domain.entities.WorkEpisodeEntity
-import jp.co.yumemi.domain.entities.WorkInfoEntity
 import tech.fika.macaron.core.contract.Result
 
 sealed class WorkDetailsResult : Result {
     data object Loading : WorkDetailsResult()
 
     data class GetWorkDetailsSuccess(
-        val workInfo: WorkInfoEntity,
         val workEpisodeList: List<WorkEpisodeEntity>
     ) : WorkDetailsResult()
 
--- kmp/presentation/work-details-shared/src/commonMain/kotlin/jp/co/yumemi/workDetails/WorkDetailsState.kt
+++ kmp/presentation/work-details-shared/src/commonMain/kotlin/jp/co/yumemi/workDetails/WorkDetailsState.kt
@@ -3,26 +3,28 @@ package jp.co.yumemi.workDetails
 import jp.co.yumemi.domain.core.DomainError
 import jp.co.yumemi.domain.entities.WorkEntity
 import jp.co.yumemi.domain.entities.WorkEpisodeEntity
-import jp.co.yumemi.domain.entities.WorkInfoEntity
 import jp.co.yumemi.utils.Parcelable
 import jp.co.yumemi.utils.Parcelize
 import tech.fika.macaron.core.contract.State
 
 sealed class WorkDetailsState : State, Parcelable {
-    @Parcelize
-    data object Initial : WorkDetailsState()
+    abstract val work: WorkEntity
 
     @Parcelize
-    data object Loading : WorkDetailsState()
+    data class Initial(override val work: WorkEntity) : WorkDetailsState()
+
+    @Parcelize
+    data class Loading(override val work: WorkEntity) : WorkDetailsState()
 
     @Parcelize
     data class Stable(
-        val workInfo: WorkInfoEntity,
+        override val work: WorkEntity,
         val workEpisodeList: List<WorkEpisodeEntity>,
     ) : WorkDetailsState()
 
     @Parcelize
     data class Error(
+        override val work: WorkEntity,
         val error: DomainError
     ) : WorkDetailsState()
 }
