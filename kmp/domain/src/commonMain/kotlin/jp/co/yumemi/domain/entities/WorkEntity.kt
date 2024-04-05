fc70dee899b0dc8c73b45e4a0372557ca54a33ff
LottieAnne <a_fukui@yumemi.co.jp>
2024-04-05 18:41:32 +0200

    feat: add domain

--- kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/entities/WorkEntity.kt
+++ kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/entities/WorkEntity.kt
@@ -4,14 +4,22 @@ import jp.co.yumemi.utils.Parcelable
 import jp.co.yumemi.utils.Parcelize
 
 /**
- * ホーム画面 一覧のアイテム
+ * ホーム画面 一覧/詳細のアイテム
  */
 @Parcelize
 data class WorkEntity(
+    /** 作品のid */
+    val id: Int,
     /** 作品のタイトル */
     val title: String,
     /** 作品のリリース時期 */
     val seasonName: String,
     /** 作品のイメージ画像 */
     val imageUrl: String?,
+    /** エピソード数 */
+    val episodes: Int,
+    /** 見てる / 見たい / 見た人の数 */
+    val watchers: Int,
+    /** レビュー数 */
+    val reviews: Int,
 ) : Parcelable
--- kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/repositories/WorkDetailsRepository.kt
+++ kmp/domain/src/commonMain/kotlin/jp/co/yumemi/domain/repositories/WorkDetailsRepository.kt
@@ -1,9 +1,7 @@
 package jp.co.yumemi.domain.repositories
 
 import jp.co.yumemi.domain.entities.WorkEpisodeEntity
-import jp.co.yumemi.domain.entities.WorkInfoEntity
 
 interface WorkDetailsRepository {
-    suspend fun getWorkInfo(): WorkInfoEntity
-    suspend fun getWorkEpisodeList(): List<WorkEpisodeEntity>
+    suspend fun getWorkEpisodeList(id: Int): List<WorkEpisodeEntity>
 }
