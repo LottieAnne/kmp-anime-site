277115574eb89af7a82af491065e51d1fd09c68e
LottieAnne <a_fukui@yumemi.co.jp>
2024-04-08 16:42:32 +0200

    feat: add home navigator

--- android/features/home/src/main/kotlin/jp/co/yumemi/features/home/HomeNavigator.kt
+++ android/features/home/src/main/kotlin/jp/co/yumemi/features/home/HomeNavigator.kt
@@ -1,7 +1,8 @@
 package jp.co.yumemi.features.home
 
 import jp.co.yumemi.core.foundation.Navigator
+import jp.co.yumemi.domain.entities.WorkEntity
 
 interface HomeNavigator: Navigator {
-    fun workDetails()
+    fun workDetails(work: WorkEntity)
 }
